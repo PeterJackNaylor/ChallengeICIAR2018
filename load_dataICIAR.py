c59ae1ddb1fbae1b8a6d8e4ac58160b96fa47554
pnaylor <peter.naylor@mines-paristech.fr>
2018-01-15 13:21:07 +0100

    update partb

---
+++ load_dataICIAR.py
@@ -0,0 +1,28 @@
+
+import pdb
+from glob import glob
+from skimage.io import imread
+from os.path import basename
+import numpy as np
+
+PATH = './samples/*.png'
+
+num_classes = 4
+num_channels = 3
+img_width = 224
+img_height = 224
+
+def load_ICIAR_data():
+    folder = glob(PATH)
+    n = len(folder)
+    X = np.zeros(shape=(n, img_height, img_width, num_channels), dtype='uint8')
+    y = np.zeros(shape=(n, num_classes), dtype='uint8')
+    for k, f in enumerate(folder):
+        img = imread(f).astype('uint8')
+        X[k] = img[:,:,0:3]
+        fname = basename(f).replace('.png', '')
+        lbl, ind = fname.split('_')
+        y[k, int(lbl)] = 1
+    return X, y
+        
+        
