01d7c6fb2742b3c43230eb9b0223b3e87ce0b309
pnaylor <peter.naylor@mines-paristech.fr>
2018-01-24 11:29:32 +0100

    update parta with save images for prediction

--- partA/ExtractProbFromTrainedRes.py
+++ partA/ExtractProbFromTrainedRes.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('agg')
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
@@ -20,6 +22,17 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import sys
 from os.path import basename
 import pdb
+import matplotlib.pyplot as plt
+
+
+
+fig = plt.figure(figsize=(16,16))
+fig.suptitle('Visualizing partB results for partA', fontsize=14, fontweight='bold')
+
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=0.85)
+
+
 
 trained_weights = sys.argv[2]
 
@@ -30,23 +43,35 @@ else:
 
 n_classes = 4
 #FACTORS = [0.1]
-FACTORS = [1., 0.75, 0.5, 0.25, 0.1]
+FACTORS = [1.] # , 0.75, 0.5, 0.25, 0.1]
 
+def vec_color(prob):
+    val = np.argmax(prob)
+    if val == 0:
+        return [255, 255, 255]
+    elif val == 1:
+        return [255, 0, 0]
+    elif val == 2:
+        return [0, 255, 0]
+    elif val == 3:
+        return [0, 0, 255]
 def sliding_window(image, stepSize, windowSize):
     # slide a window across the imag
-    for y in xrange(0, image.shape[0] - windowSize[0] + stepSize, stepSize):
-        for x in xrange(0, image.shape[1] - windowSize[1] + stepSize, stepSize):
+    for x in xrange(0, image.shape[0] - windowSize[0] + stepSize, stepSize):
+        for y in xrange(0, image.shape[1] - windowSize[1] + stepSize, stepSize):
             # yield the current window
-            res_img = image[y:y + windowSize[1], x:x + windowSize[0]]
+            print x, y
+            res_img = image[x:x + windowSize[0], y:y + windowSize[0]]
             change = False
-            if res_img.shape[0] != windowSize[1]:
-                y = image.shape[0] - windowSize[1]
+            if res_img.shape[0] != windowSize[0]:
+                x = image.shape[0] - windowSize[0]
                 change = True
-            if res_img.shape[1] != windowSize[0]:
-                x = image.shape[1] - windowSize[0]
+            if res_img.shape[1] != windowSize[1]:
+                y = image.shape[1] - windowSize[1]
                 change = True
             if change:
-                res_img = image[y:y + windowSize[1], x:x + windowSize[0]]
+                res_img = image[x:x + windowSize[0], y:y + windowSize[1]]
+                print "Changed:", x, y
             yield (x, y, x + windowSize[0], y + windowSize[1], res_img)
 
 
@@ -62,19 +87,7 @@ labels = list(set(flatten([l.split(' ') for l in lbl])))
 label_map = {l: i for i, l in enumerate(labels)}
 inv_label_map = {i: l for l, i in label_map.items()}
 
-# # use ResNet50 model extract feature from fc1 layer
-# base_model = ResNet50(weights='imagenet', pooling=max, include_top = False)
-# input = Input(shape=(224,224,3),name = 'image_input')
-# x = base_model(input)
-# x = Flatten()(x)
-# model = Model(inputs=input, outputs=x)
-
-#probability to concat
 base_model = load_model(trained_weights)
-#input = Input(shape=(224,224,3), name='image_input')
-#x = base_model(input)
-#x = Flatten()(x)
-#model_prob = Model(inputs=input, outputs=x)
 model_prob = base_model
 X_mat = []
 y_mat = []
@@ -104,14 +117,25 @@ for fact in FACTORS:
     # img_scale = img_scale - mean
     stepSize = 224
     windowSize = (224, 224)
-    for x, y, x_e, y_e, x in sliding_window(img_scale, stepSize, windowSize):
+    for x_b, y_b, x_e, y_e, x in sliding_window(img_scale, stepSize, windowSize):
         x = x.astype(float)
         x = np.expand_dims(x, axis=0)
     #    x = preprocess_input(x)
 
         features = model_prob.predict(x) 
+        color = vec_color(features)
+        image[x_b:x_e,y_b:(y_b+2)] = color
+        image[x_b:x_e,(y_e-2):y_e] = color
+        image[x_e:(x_e+2),y_b:y_e] = color
+        image[(x_b-2):x_b,y_b:y_e] = color
+        mid_x = (x_b + x_e) / 2 
+        mid_y = (y_b + y_e) / 2 - 100
+        ax.text(mid_y, mid_x, '{:10.2f}'.format(features.max()), fontsize=15)
         features_reduce =  features.squeeze()
         img_feat_list.append(features_reduce)
+ax.imshow(image)
+fig.axis('off')
+fig.savefig(tags.replace('.tif', '_prob.tif'), bbox_inches='tight')
 matrix_img_feat = np.column_stack(img_feat_list)
 for i in range(matrix_img_feat.shape[0]):
     matrix_img_feat[i] = np.sort(matrix_img_feat[i])
